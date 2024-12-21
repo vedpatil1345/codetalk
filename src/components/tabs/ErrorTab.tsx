f87a3816ff571565976019b416b3c161a56fc112
vedpatil1345 <vedpatil1345@gmail.com>
2024-12-21 23:11:02 +0530

    home

--- src/components/tabs/ErrorTab.tsx
+++ src/components/tabs/ErrorTab.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Card } from '@/components/ui/card';
 import { AnalysisResults } from '../analysis/AnalysisResults';
