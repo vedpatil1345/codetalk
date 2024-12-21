f63ebab812a1fd9adfb096562b0952683dffd1c1
vedpatil1345 <vedpatil1345@gmail.com>
2024-12-21 19:36:08 +0530

    home

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -86,8 +86,7 @@ const HomePage = () => {
             <div className="flex justify-between h-16 items-center">
               <div className="flex-shrink-0 flex items-center">
                 <h1 className="text-2xl font-bold flex items-center text-blue-700 dark:text-blue-300">
-                  Code
-                  <TypewriterText />
+                  CodeTalk
                 </h1>
               </div>
               <div className="flex items-center gap-4">
@@ -113,7 +112,11 @@ const HomePage = () => {
         {/* Hero Section */}
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="text-center">
-            <h1 className="text-4xl font-bold sm:text-6xl mb-6">
+          <h1 className=" text-7xl font-bold flex justify-center text-blue-700 dark:text-blue-300">
+                  Code
+                  <TypewriterText />
+                </h1>
+            <h1 className="text-4xl font-bold sm:text-6xl mt-6 mb-6">
               The Online Code Analysis Platform
             </h1>
             <p className="text-xl text-muted-foreground mb-8">
